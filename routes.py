ea092a7a7860d2c2c34dd5a53d1cca35d890bab1
lillee205 <leelillian205@gmail.com>
2021-06-11 10:52:54 -0700

    fixed filtering system, fixed routing errors so that you can type in url for specific problem, problems on index page are no longer hard coded, reorganization of code

--- routes.py
+++ routes.py
@@ -7,7 +7,7 @@ import json
 from problems import *
 app = Flask(__name__)
 
-#stores the current problem we are on
+# stores the current problem we are on
 problem = ""
 contents = ""
 
@@ -17,41 +17,37 @@ def index():
     global problem
     global contents
 
+    problem = ""
+    contents = ""
+
     if request.method == "POST":
         problem = request.form.to_dict()['hiddenChoose']
         if problem in probDict:
-            contents = probDict[problem] 
-        else:
-            return render_template("index.html") # if key error, go back to index html
-        return render_template("writeCode.html", function_name=contents['func_name'], description=contents['desc'], tests=contents['testInputs'], ansKey=contents['testInputAnswers'], func_call=contents['func_call'])
-    else:
-        return render_template('index.html')
+            contents = probDict[problem]
+
+    return render_template('index.html', problems = probDict)
 
 
-@app.route('/change_type', methods=["POST", "GET"])
-def change_type():
+@app.route('/<problem_pg>', methods=["POST", "GET"])
+def change_type(problem_pg):
     global problem
     global contents
-
+    if (problem_pg) in probDict:
+        problem = problem_pg
+        contents = probDict[problem]
     if request.method == "POST":
         newView = request.form.to_dict()['hiddenSelect']
+
         if newView == "test inputs":
-            return render_template("testInputs.html", function_name=contents['func_name'], description=contents['desc'], tests=contents['testInputs'], ansKey=contents['testInputAnswers'], func_call=contents['func_call'])
+            return render_template("testInputs.html", function_name=contents['func_name'], description=contents['desc'], tests=contents['testInputs'], ansKey=contents['testInputAnswers'], func_call=contents['func_call'], tags = contents['tags'])
         elif newView == "write code":
-            return render_template("writeCode.html", function_name=contents['func_name'], description=contents['desc'], tests=contents['testInputs'], ansKey=contents['testInputAnswers'], func_call=contents['func_call'])
+            return render_template("writeCode.html", function_name=contents['func_name'], description=contents['desc'], tests=contents['testInputs'], ansKey=contents['testInputAnswers'], func_call=contents['func_call'], tags = contents['tags'])
         elif newView == "test outputs":
-            return render_template("testOutputs.html", function_name=contents['func_name'], description=contents['desc'], tests=contents['testInputs'], ansKey=contents['testInputAnswers'], func_call=contents['func_call'])
-        elif newView == "home":
-            problem = ""
-            contents = ""
-            return render_template("index.html")
-        elif newView == "rand":
-            rand = random.choice(list(probDict.keys()))
-            problem = rand
-            contents = probDict[problem]
-            return render_template("writeCode.html", function_name=contents['func_name'], description=contents['desc'], tests=contents['testInputs'], ansKey=contents['testInputAnswers'], func_call=contents['func_call'])
+            return render_template("testOutputs.html", function_name=contents['func_name'], description=contents['desc'], tests=contents['testInputs'], ansKey=contents['testInputAnswers'], func_call=contents['func_call'], tags = contents['tags'])
         else:
             return render_template("index.html")
+
+    return render_template("writeCode.html", function_name=contents['func_name'], description=contents['desc'], tests=contents['testInputs'], ansKey=contents['testInputAnswers'], func_call=contents['func_call'], tags = contents['tags'])
 
 
 @app.route('/background_process_writeCode')
@@ -59,12 +55,11 @@ def background_process_writeCode():
     code = request.args.get('code')
     try:
         exec(code)
-        print(request.args.get('inputs'))
         testInputsJSON = json.loads(request.args.get('inputs'))
         testAns = pcode.checkAnswers(code, testInputsJSON)
         return jsonify(result=testAns)
     except Exception as e:
-        #traceback.print_exc()
+        # traceback.print_exc()
         return jsonify(result="Error: " + str(e))
 
 
@@ -72,16 +67,16 @@ def background_process_writeCode():
 def background_process_testInputs():
 
     yourOutput = json.loads(request.args.get('output'))
-    input = json.loads(request.args.get('input'))
+    input = request.args.get('input')
     try:
         correctAns = eval(
-            "pcode." + contents[4][: (contents[4].index("("))] + "(" + str(input) + ")")
+            "pcode." + contents['func_call'][: (contents['func_call'].index("("))] + "(" + input + ")")
         if correctAns == yourOutput:
             return jsonify(result="true")
         else:
             return jsonify(result="false")
     except Exception as e:
-        #traceback.print_exc()
+        traceback.print_exc()
         return jsonify(result="Error")
 
 
